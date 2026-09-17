f3d285ffa82231debbd63ee56eaf3fca03867bac
matthiola <liyouwei654321@gmail.com>
2026-09-16 18:11:15 -0700

    parking lot

---
+++ src/main/java/ood/parkingLot/ParkingLot.java
@@ -0,0 +1,18 @@
+package ood.parkingLot;
+//Design a parking lot system where different types of vehicles can park,
+// and the system manages spot assignment and calculates fees upon exit
+public class ParkingLot {
+    //process
+    //  a man can request parking , the system will check avaliable spots for him, if vancan, it will going let user to input his lisence number,
+    //and car size, then the system will give a recepit with spot for him
+
+    //entity                            ->                                       //behavior
+    //car(lisense number, size)                                                 //requestParking
+    //parking spot（size(small, middle, big), avaliable or not, spot number）    //find avalibale parking spot base on the car size
+    //fee(money,leave time， receipt)                                           //fee calculate base on the housr and size of the car and pay
+    //receipt（lisense number，spot number, start time， receipt number）          //  get a receipt when coming
+
+
+
+
+}
